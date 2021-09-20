34c2ed4a421ad7f17a32ba5c262d8836ff23cf2a
obata junya <xgjhs932qoo@yahoo.co.jp>
2021-09-20 03:09:23 +0000

    0920

--- resources/views/admin/profile/edit.blade.php
+++ resources/views/admin/profile/edit.blade.php
@@ -1,1 +1,16 @@
-<!---->
+{{-- layouts/admin.blade.php --}}
+@extends('layouts.edit')
+
+{{-- admin.blade.phpのyield('title')に'ニュースの新規作成'を埋め込む　--}}
+@section('title','MYエディット')
+
+{{-- admin.blade.phpの@yield('content')に以下にタグを埋め込み --}}
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>MYエディット</h2>
+            </div>
+        </div>
+    </div>
+@endsection
